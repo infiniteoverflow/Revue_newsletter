9e98fd967bb10d84a27b915cb1aa21e86db628e6
Aswin Gopinathan <40236624+infiniteoverflow@users.noreply.github.com>
2022-05-11 08:52:44 +0530

    Refactored codebase

--- bin/server.dart
+++ bin/server.dart
@@ -1,24 +1,8 @@
 import 'dart:io';
 
-import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
-import 'package:shelf_router/shelf_router.dart';
 
 import 'revue_newsletter.dart';
-
-// Configure routes.
-final _router = Router()
-  ..get('/', _rootHandler)
-  ..get('/echo/<message>', _echoHandler);
-
-Response _rootHandler(Request req) {
-  return Response.ok('Hello, World!\n');
-}
-
-Response _echoHandler(Request request) {
-  final message = params(request, 'message');
-  return Response.ok('$message\n');
-}
 
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
@@ -26,6 +10,8 @@ void main(List<String> args) async {
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  // Defining a custom handler
   final server = await serve(RevueNewsletter().handler, ip, port);
   print('Server listening on port ${server.port}');
 }
